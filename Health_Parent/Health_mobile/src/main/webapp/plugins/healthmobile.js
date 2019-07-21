f01ee9ff60019f188ae95d50eebf78c06d1a9cdb
zdl <775880553@qq.com>
2019-07-21 20:03:34 +0800

    aaa

--- Health_Parent/Health_mobile/src/main/webapp/plugins/healthmobile.js
+++ Health_Parent/Health_mobile/src/main/webapp/plugins/healthmobile.js
@@ -1,5 +1,6 @@
 //获取指定的URL参数值 http://localhost/pages/setmeal_detail.html?id=3&name=jack
 function getUrlParam(paraName) {
+<<<<<<< Updated upstream
 
     //获取url地址中?之后包括?的内容并转成字符串
     var url = document.location.search.toString();
@@ -17,6 +18,24 @@ function getUrlParam(paraName) {
         } else {
             return null;
         }
+=======
+    var url = document.location.toString();
+    //alert(url);
+    var arrObj = url.split("?");//id=3    name=jack
+    if (arrObj.length > 1) {
+        var arrPara = arrObj[1].split("&");//数组
+        var arr;
+        for (var i = 0; i < arrPara.length; i++) {
+            arr = arrPara[i].split("=");
+            if (arr != null && arr[0] == paraName) {
+                return arr[1];
+            }
+        }
+        return "";
+    }
+    else {
+        return "";
+>>>>>>> Stashed changes
     }
 }
 
@@ -30,7 +49,11 @@ function getToday() {
 }
 
 //获得指定日期后指定天数的日期
+<<<<<<< Updated upstream
 function getSpecifiedDate(date, days) {
+=======
+function getSpecifiedDate(date,days) {//2019-07-31
+>>>>>>> Stashed changes
     date.setDate(date.getDate() + days);//获取指定天之后的日期
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
@@ -45,7 +68,11 @@ function getSpecifiedDate(date, days) {
  3--最后以0-9的9个整数结尾。
  */
 function checkTelephone(telephone) {
+<<<<<<< Updated upstream
     var reg = /^[1][3,4,5,7,8][0-9]{9}$/;
+=======
+    var reg=/^[1][3,4,5,7,8][0-9]{9}$/;
+>>>>>>> Stashed changes
     if (!reg.test(telephone)) {
         return false;
     } else {
@@ -57,11 +84,19 @@ function checkTelephone(telephone) {
  * 身份证号码校验
  * 身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X
  */
+<<<<<<< Updated upstream
 function checkIdCard(idCard) {
     var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
     if (reg.test(idCard)) {
         return true;
     } else {
+=======
+function checkIdCard(idCard){
+    var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    if(reg.test(idCard)){
+        return true;
+    }else{
+>>>>>>> Stashed changes
         return false;
     }
 }
@@ -69,7 +104,10 @@ function checkIdCard(idCard) {
 var clock = '';//定时器对象，用于页面30秒倒计时效果
 var nums = 30;
 var validateCodeButton;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 //基于定时器实现30秒倒计时效果
 function doLoop() {
     validateCodeButton.disabled = true;//将按钮置为不可点击
@@ -77,7 +115,11 @@ function doLoop() {
     if (nums > 0) {
         validateCodeButton.value = nums + '秒后重新获取';
     } else {
+<<<<<<< Updated upstream
         clearInterval(clock); //清除js定时器
+=======
+        window.clearInterval(clock); //清除js定时器
+>>>>>>> Stashed changes
         validateCodeButton.disabled = false;
         validateCodeButton.value = '重新获取验证码';
         nums = 30; //重置时间
